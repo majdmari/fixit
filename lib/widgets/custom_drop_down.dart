d4425c011bc4e97d07026be573370848e76b12b6
majdmari <majdabueliass@gmail.com>
2024-03-13 18:42:27 +0300

    last

--- lib/widgets/custom_drop_down.dart
+++ lib/widgets/custom_drop_down.dart
@@ -15,7 +15,7 @@ class CustomDropdown<T> extends StatefulWidget {
     required this.labelText,
     required this.initialValue,
     required this.onChanged,
-    this.dropdownMenuBackgroundColor = Colors.white,
+    this.dropdownMenuBackgroundColor = Colors.red,
   }) : super(key: key);
 
   @override
