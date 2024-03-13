7aa02df812dca3ca61d70c9ada0455c1b70af232
majdmari <majdabueliass@gmail.com>
2024-03-13 16:59:37 +0300

    last color

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -26,7 +26,7 @@ class LoginScreen extends StatelessWidget {
                   child: Text(
                     'Login----------------------',
                     style: TextStyle(
-                      color: Colors.green,
+                      color: Colors.blue,
                       fontSize: 40,
                     ),
                   ),
