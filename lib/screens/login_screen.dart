b017383187b1f1ee7606c5769fd9540b87e2d210
majdmari <majdabueliass@gmail.com>
2024-03-13 23:58:55 +0300

    last

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,7 +11,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: KSurface,
+      backgroundColor: Colors.red,
       body: Padding(
         padding: const EdgeInsets.all(30),
         child: ListView(
